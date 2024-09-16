b63b6e2b241fa47624a366427119c8134a6b68ef
BenSnaith <bsnaith.bs@icloud.com>
2024-09-16 22:50:13 +0100

    initial commit

---
+++ src/main/java/net/riusab/rtt/RTTClient.java
@@ -0,0 +1,10 @@
+package net.riusab.rtt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RTTClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/riusab/rtt/RiusabsTimeTravelDataGenerator.java
@@ -0,0 +1,11 @@
+package net.riusab.rtt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RiusabsTimeTravelDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
